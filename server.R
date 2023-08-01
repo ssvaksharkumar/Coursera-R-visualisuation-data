4f0c231c1d678948e29edfcc491a16f19dd21441
ssvaksharkumar <134367122+ssvaksharkumar@users.noreply.github.com>
2023-08-01 12:27:36 +0200

    Update server.R

--- server.R
+++ server.R
@@ -18,20 +18,20 @@ shinyServer(function(input, output) {
   
   # TASK 5: Create logic to plot histogram or boxplot
   output$p1 <- renderPlot({
+    data <- read.csv("adult.csv")
     if (input$graph_type == "histogram") {
       # Histogram
-      ggplot(df_country(), aes_string(x = input$continous_variables)) +
-        geom_histogram() +  # histogram geom
-        ylab="Number of people"+ # labels
+      ggplot(df_country(), aes_string(x = input$radio_continous)) +
+        geom_histogram(bins=30) +  # histogram geom
+        labs(title=paste("Trend of",input$radio_continous),y="Number of people")+ # labels
         facet_wrap(~prediction)
-    }
-           # facet by prediction
+    } # facet by prediction
     else {
       # Boxplot
-      ggplot(df_country(), aes_string(y = input$continous_variables)) +
+      ggplot(df_country(), aes_string(y = input$radio_continous)) +
         geom_boxplot() +  # boxplot geom
         coord_flip() +  # flip coordinates
-        ylab="Number of people" +  # labels
+        labs(title=paste("How",input$radio_continous,"value is spread"),y="Number of people",x="Age")+   # labels
         facet_wrap(~prediction)   # facet by prediction
     }
     
@@ -40,17 +40,18 @@ shinyServer(function(input, output) {
   
    #TASK 6: Create logic to plot faceted bar chart or stacked bar chart
   output$p2 <- renderPlot({
+    data <- read.csv("adult.csv")
      #Bar chart
-    p <- ggplot(df_country(), aes_string(x = input$categorical_variables)) +
-      ylab="Number of people" +  # labels
-      theme(axis.test.x=element_text(angle=45),legend.position = "left" )   # modify theme to change text angle and legend position
+    p <- ggplot(df_country(), aes(x = input$radio_categorical)) +
+     labs(title=paste("Trend of",input$radio_categorical),y="Number of people",x="Age") + # labels
+     theme(axis.text.x= element_text(angle=45),legend.position = "bottom")   # modify theme to change text angle and legend position
     
     if (input$is_stacked) {
       p + geom_bar(fill="prediction")  # add bar geom and use prediction as fill
     }
     else{
       p + 
-        geom_bar(fill=input$categorical_variables) + # add bar geom and use input$categorical_variables as fill 
+        geom_bar(fill=input$radio_categorical) + # add bar geom and use input$categorical_variables as fill 
         facet_wrap(~prediction)   # facet by prediction
     }
   })
